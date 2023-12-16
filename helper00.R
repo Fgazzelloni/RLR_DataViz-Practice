92a37455c1a3544e7d378c265055529a9fdecf7e
fgazzelloni <docksbox@pm.me>
2023-12-16 13:17:13 +0000

    helper00

--- helper00.R
+++ helper00.R
@@ -1,12 +1,18 @@
 ## Send your changes to GitHub
 
+# Procedere all'autenticazione
+# IN TERMINAL
+git config --global user.email "you@example.com"
+git config --global user.name "Your User Name"
+
+# IN CONSOLE
 install.packages("usethis")
 install.packages("gitcreds")
 usethis::create_github_token() # apre una pagina git dove prendere il token
 gitcreds::gitcreds_set() # inserisci token
 
-usethis::pr_init("<la-tua-nuova-branch>") # iniziazione della nuova branch (informa git che c'è una nuova branch)
+# iniziazione della nuova branch (informa git che c'è una nuova branch)
+usethis::pr_init("<la-tua-nuova-branch>") 
+
 # commit i cambiamenti fatti e poi inviali
 usethis::pr_push() # manda i cambiamenti
-
-# commit all your files
